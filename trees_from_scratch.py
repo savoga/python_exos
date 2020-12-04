8d02ddff9872127c91077c13f59a3f7dd38cadb7
savoga <savoure.gael@gmail.com>
2020-12-04 23:35:41 +0100

    add max_depth and comparison with scikit-learn

--- trees_from_scratch.py
+++ trees_from_scratch.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-training_data = [ # = ROWS of the dataset
-    ['France', 5, 'Premium'],
-    ['Switzerland', 5, 'Premium'],
-    ['UAE', 10, 'Advanced'],
-    ['UAE', 10, 'Advanced'],
-    ['Switzerland', 5, 'Basic'],
-]
-header = ["Country", "Wealth", "Mandate"]
-
-# ********************* SUPPLEMENTARY FUNCTIONS ************
 def label_counts(rows):
     """Counts the number of each label in a dataset"""
     counts = {}
@@ -63,60 +53,66 @@ class Decision_Node:
 # ************************************************************
 
 # ********************* FUNCTIONS TO BUILD THE TREE **********
-def partition(rows, split_element):
-    """For each row in the dataset, check if it matches the question. If
-    so, add it to 'true rows', otherwise, add it to 'false rows' """
-    left_rows, right_rows = [], []
-    for row in rows:
-        if split_element.compare(row):
-            right_rows.append(row)
-        else:
-            left_rows.append(row)
-    return right_rows, left_rows
+class DecisionTree:
+    def __init__(self, max_depth):
+        self.max_depth = max_depth
+        self.count_level = 0
 
-def gini(rows):
-    """Calculate the Gini Impurity for a list of rows.
-    https://en.wikipedia.org/wiki/Decision_tree_learning#Gini_impurity"""
-    counts = label_counts(rows)
-    impurity = 1
-    for lbl in counts:
-        prob_of_lbl = counts[lbl] / float(len(rows))
-        impurity -= prob_of_lbl**2
-    return impurity
+    def partition(self, rows, split_element):
+        """For each row in the dataset, check if it matches the question. If
+        so, add it to 'true rows', otherwise, add it to 'false rows' """
+        left_rows, right_rows = [], []
+        for row in rows:
+            if split_element.compare(row):
+                right_rows.append(row)
+            else:
+                left_rows.append(row)
+        return right_rows, left_rows
 
-def info_gain(left, right, current_uncertainty):
-    p = float(len(left)) / (len(left) + len(right))
-    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+    def gini(self, rows):
+        """Calculate the Gini Impurity for a list of rows.
+        https://en.wikipedia.org/wiki/Decision_tree_learning#Gini_impurity"""
+        counts = label_counts(rows)
+        impurity = 1
+        for lbl in counts:
+            prob_of_lbl = counts[lbl] / float(len(rows))
+            impurity -= prob_of_lbl**2
+        return impurity
 
-def find_best_split(rows):
-    """Find the best SplitElement by iterating over every feature / value
-    and calculating the information gain."""
-    best_gain = 0
-    best_split_element = None
-    current_uncertainty = gini(rows)
-    n_features = len(rows[0]) - 1
-    for col in range(n_features):
-        values = set([row[col] for row in rows])
-        for val in values:
-            split_element = SplitElement(col, val)
-            right_rows, left_rows = partition(rows, split_element)
-            if len(right_rows) == 0 or len(left_rows) == 0:
-                continue
-            gain = info_gain(right_rows, left_rows, current_uncertainty)
-            if gain >= best_gain:
-                best_gain, best_split_element = gain, split_element
-    return best_gain, best_split_element
+    def info_gain(self, left, right, current_uncertainty):
+        p = float(len(left)) / (len(left) + len(right))
+        return current_uncertainty - p * self.gini(left) - (1 - p) * self.gini(right)
 
-def build_tree(rows):
-    gain, split_element = find_best_split(rows)
-    if gain == 0:
-        return Leaf(rows)
-    right_rows, left_rows = partition(rows, split_element)
-    # Recursively build the true branch.
-    right_branch = build_tree(right_rows)
-    # Recursively build the false branch.
-    left_branch = build_tree(left_rows)
-    return Decision_Node(split_element, right_branch, left_branch)
+    def find_best_split(self, rows):
+        """Find the best SplitElement by iterating over every feature / value
+        and calculating the information gain."""
+        best_gain = 0
+        best_split_element = None
+        current_uncertainty = self.gini(rows)
+        n_features = len(rows[0]) - 1
+        for col in range(n_features):
+            values = set([row[col] for row in rows])
+            for val in values:
+                split_element = SplitElement(col, val)
+                right_rows, left_rows = self.partition(rows, split_element)
+                if len(right_rows) == 0 or len(left_rows) == 0:
+                    continue
+                gain = self.info_gain(right_rows, left_rows, current_uncertainty)
+                if gain >= best_gain:
+                    best_gain, best_split_element = gain, split_element
+        return best_gain, best_split_element
+
+    def build_tree(self, rows): # =fit()
+        gain, split_element = self.find_best_split(rows)
+        if gain == 0 or self.count_level==self.max_depth:
+            return Leaf(rows)
+        right_rows, left_rows = self.partition(rows, split_element)
+        # Recursively build the true branch.
+        self.count_level += 1
+        right_branch = self.build_tree(right_rows)
+        # Recursively build the false branch.
+        left_branch = self.build_tree(left_rows)
+        return Decision_Node(split_element, right_branch, left_branch)
 # **********************************************************
 
 def print_tree(node, spacing=""):
@@ -128,3 +124,32 @@ def print_tree(node, spacing=""):
     print_tree(node.right_branch, spacing + "  ")
     print (spacing + '--> False:')
     print_tree(node.left_branch, spacing + "  ")
+
+# ********************* COMPARISON WITH SCIKIT LEARN ************
+
+training_data = [ # = ROWS of the dataset
+    ['France', 5, 'Premium'],
+    ['Switzerland', 5, 'Premium'],
+    ['UAE', 10, 'Advanced'],
+    ['UAE', 10, 'Advanced'],
+    ['Switzerland', 5, 'Basic'],
+    ['France', 12, 'Basic']
+]
+header = ["Country", "Wealth", "Mandate"]
+
+my_tree = DecisionTree(max_depth=2)
+print_tree(my_tree.build_tree(training_data))
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import tree
+
+df_training_data = pd.DataFrame(data=training_data, columns=header)
+clf = DecisionTreeClassifier(random_state=0, max_depth=2)
+ohe = OneHotEncoder()
+X = ohe.fit_transform(df_training_data[['Country']])
+X = pd.concat([pd.DataFrame(data=X.toarray()),df_training_data['Wealth']], axis=1)
+y = df_training_data['Mandate']
+clf.fit(X, y)
+tree.plot_tree(clf)
