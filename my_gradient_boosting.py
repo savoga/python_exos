41383fd3bc405acf0b9582eccd7dd06d6ff88cb7
savoga <savoure.gael@gmail.com>
2020-12-13 18:32:26 +0100

    enhance gradient boosting

--- my_gradient_boosting.py
+++ my_gradient_boosting.py
@@ -1,31 +1,66 @@
+'''
+In this implementation, x has one dimension only.
+x=[1,2,3,...,29,30] NEVER CHANGES
+y_target=random numbers
+F=y_est is updated at each loop
+'''
+
 import trees_from_scratch as tfs
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+x = np.arange(0,30)
+x = pd.DataFrame({'x':x})
 
-x1 = np.random.normal(0,1,100)
-x2 = np.random.normal(0,1,100)
-x3 = np.random.normal(0,1,100)
-x = np.stack([x1,x2,x3], axis=1)
+# just random uniform distributions in differnt range
 
-y = np.random.randint(0,10,100)
+y1 = np.random.uniform(10,15,10)
+y2 = np.random.uniform(0,5,10)
+y3 = np.random.uniform(13,17,10)
+
+y_target = np.concatenate((y1,y2,y3))
+
+plt.figure()
+plt.plot(x,y_target, 'o')
 
 n = x.shape[0]
 p = x.shape[1]
 
 # 1. Initial function = mean of y
-F_0 = np.ones(n)*np.mean(y)
-n_estimators = 10
+F_0 = np.ones(n)*np.mean(y_target)
+n_estimators = 1
+
+plt.plot(x,F_0,'r')
+
+F = F_0
 
 for i in range(n_estimators):
 
     # 2. compute pseudo-residuals
-    res = F_0 - y
+    res = y_target - F_0
     res = res.reshape(len(res),1)
 
     # 3. train base learner
-    tree = tfs.build_tree(np.concatenate([x, res], axis=1))
+    tree = tfs.DecisionTree(max_depth=3)
+    tree = tree.build_tree(np.concatenate([x, res], axis=1)) # we predict the residuals
+    # TODO: replace with a tree that can handle REGRESSION
+    split_element = tree.split_element.value
 
-    # 4. find gradient step
-    # derive loss function?
+    left_idx = np.where(x <= split_element)[0]
+    right_idx = np.where(x > split_element)[0]
 
-    # 5. update function
+    left_mean = np.array(x)[left_idx].mean()
+    right_mean = np.array(x[right_idx]).mean()
+
+    pred = np.ones((len(x),1))
+    pred[left_idx] = left_mean
+    pred[right_idx] = right_mean
+
+    # 4. update function
+    F = F + pred
+
+    # plotting after prediction
+    plt.figure()
+    plt.plot(x,y_target, 'o')
+    plt.plot(x, F)
