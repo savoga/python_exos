1dcc228ea8375262a299c2cd8737246b61643b4b
Gaël Savouré <savoure.gael@gmail.com>
2022-10-27 11:07:04 +0200

    add source

--- lda-from-scratch.py
+++ lda-from-scratch.py
@@ -1,3 +1,5 @@
+# Implementation from https://gist.github.com/umbertogriffo/5041b9e4ec6c3478cef99b8653530032
+
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
