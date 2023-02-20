b8477a62a97359bc9dc25a0deddebd33f87b2f6f
Gaël Savouré <savoure.gael@gmail.com>
2023-02-20 19:31:09 +0100

    embeddings - word2vec

---
+++ embedding-word2vec.py
@@ -0,0 +1,114 @@
+import re
+import numpy as np
+
+# https://jaketae.github.io/study/word2vec/
+
+# text = '''Machine learning is the study of computer algorithms that \
+# improve automatically through experience. It is seen as a \
+# subset of artificial intelligence. Machine learning algorithms \
+# build a mathematical model based on sample data, known as \
+# training data, in order to make predictions or decisions without \
+# being explicitly programmed to do so. Machine learning algorithms \
+# are used in a wide variety of applications, such as email filtering \
+# and computer vision, where it is difficult or infeasible to develop \
+# conventional algorithms to perform the needed tasks.'''
+
+text = 'I like gambling every day.'
+
+def tokenize(text):
+    pattern = re.compile(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
+    return pattern.findall(text.lower())
+
+tokens = tokenize(text)
+
+def mapping(tokens):
+    word_to_id = {}
+    id_to_word = {}
+    
+    for i, token in enumerate(tokens): #set(tokens)
+        word_to_id[token] = i
+        id_to_word[i] = token
+    
+    return word_to_id, id_to_word
+
+word_to_id, id_to_word = mapping(tokens)
+
+np.random.seed(42)
+
+def concat(*iterables):
+    for iterable in iterables:
+        yield from iterable
+
+def one_hot_encode(id, vocab_size):
+    res = [0] * vocab_size
+    res[id] = 1
+    return res
+
+
+def generate_training_data(tokens, word_to_id, window):
+    X = []
+    y = []
+    n_tokens = len(tokens)
+    
+    for i in range(n_tokens):
+        idx = concat(
+            range(max(0, i - window), i), 
+            range(i, min(n_tokens, i + window + 1))
+        )
+        for j in idx:
+            if i == j:
+                continue
+            X.append(one_hot_encode(word_to_id[tokens[i]], len(word_to_id)))
+            y.append(one_hot_encode(word_to_id[tokens[j]], len(word_to_id)))
+    
+    return np.asarray(X), np.asarray(y)
+
+X, y = generate_training_data(tokens, word_to_id, 2)
+
+def init_network(vocab_size, n_embedding):
+    model = {
+        "w1": np.random.randn(vocab_size, n_embedding),
+        "w2": np.random.randn(n_embedding, vocab_size)
+    }
+    return model
+
+model = init_network(len(word_to_id), 10)
+
+def softmax(X):
+    res = []
+    for x in X:
+        exp = np.exp(x)
+        res.append(exp / exp.sum())
+    return res
+
+def forward(model, X, return_cache=True):
+    cache = {}
+    
+    cache["a1"] = X @ model["w1"]
+    cache["a2"] = cache["a1"] @ model["w2"]
+    cache["z"] = softmax(cache["a2"])
+    
+    if not return_cache:
+        return cache["z"]
+    return cache
+
+def cross_entropy(z, y):
+    return - np.sum(np.log(z) * y)
+
+
+def backward(model, X, y, alpha):
+    cache  = forward(model, X)
+    da2 = cache["z"] - y
+    dw2 = cache["a1"].T @ da2
+    da1 = da2 @ model["w2"].T
+    dw1 = X.T @ da1
+    assert(dw2.shape == model["w2"].shape)
+    assert(dw1.shape == model["w1"].shape)
+    model["w1"] -= alpha * dw1
+    model["w2"] -= alpha * dw2
+    return cross_entropy(cache["z"], y)
+
+n_iter = 50
+learning_rate = 0.05
+
+history = [backward(model, X, y, learning_rate) for _ in range(n_iter)]
